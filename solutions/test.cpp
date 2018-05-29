200944a1d8c91449301a3410dbbb0be0da63f1d5
apurvparekh30 <apurvparekh30@gmail.com>
2018-05-28 20:04:16 -0400

    28thMay

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,47 +1,32 @@
-#include <stdio.h>
-#include <map>
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
+#include <cmath>
 using namespace std;
-int r[100005], p[100005];
-void init(int n) {
-    for(int i = 0; i <= n; i++)
-        r[i] = 1, p[i] = i;
-}
-int findp(int x) {
-    return p[x] == x ? x : (p[x]=findp(p[x]));
-}
-int joint(int x, int y) {
-    x = findp(x), y = findp(y);
-    if(x != y) {
-        if(r[x] > r[y]) {
-            r[x] += r[y], p[y] = x;
-            return r[x];
-        }
-        r[y] += r[x], p[x] = y;
-        return r[y];
-    }
-    return r[x];
-}
+
 int main() {
-    int t, n, tx, ty;
-    char x[30], y[30];
-    scanf("%d", &t);
-    while(t--) {
-        scanf("%d", &n);
-        init(min(2*n, 100000));
-        map<string, int> R;
-        int size = 0;
-        while(n--) {
-            scanf("%s %s", &x, &y);
-            tx = R[x];
-            if(tx == 0)
-                R[x] = ++size, tx = size;
-            ty = R[y];
-            if(ty == 0)
-                R[y] = ++size, ty = size;
-            printf("%d\n", joint(tx, ty));
-        }
-    }
-    return 0;
+	int a, b, c;
+
+	for (a = 1; 4 * a <= 2000; a++) {
+		for (b = a; a + 3 * b <= 2000; b++) {
+			for (c = b; a + b + 2 * c <= 2000; c++) {
+				long long p = (a * b * c);
+				long long s = (a + b + c);
+
+				if (p <= 1000000)
+					continue;
+
+				long long d = (1000000 * s) / (p - 1000000);
+
+				if (d < c || s + d > 2000)
+					continue;
+
+				if (fabs((a + b + c + d) / 100.0 - (a * b * c * d) / 100000000.0) < 1e-9) {
+					printf("%0.2lf %0.2lf %0.2lf %0.2lf\n", a / 100.0,
+							b / 100.0, c / 100.0, d / 100.0);
+				}
+			}
+
+		}
+	}
+
+	return 0;
 }
