445deb6a64f973c9a29d12a3e2b9e100574bc666
apurvparekh30 <apurvparekh30@gmail.com>
2018-06-15 18:34:36 -0400

    backtracking - a match making problem

--- solutions/p12210.cpp
+++ solutions/p12210.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 int main() {
     int B,S;
+    int tc = 0;
     while(scanf("%d %d",&B,&S), B||S) {
-        int bachlors[B];
-        int spinster[S];
+        tc++;
+        int minBoy = INT_MAX;
         for(int i=0;i<B;i++){
-            scanf("%d",&bachlors[i]);
+            int newBoy;
+            scanf("%d",&newBoy);
+            minBoy = min(minBoy,newBoy);
         }
         for(int i=0;i<S;i++){
-            scanf("%d",&spinster[i]);
+            int newGirl;
+            scanf("%d",&newGirl);
         }
-        
+        if(B <= S){
+            printf("Case %d: 0\n",tc);
+        }
+        else {
+            printf("Case %d: %d %d\n",tc,B-S,minBoy);
+        }
     }
     return 0;
 }
