819aa95134316976faff5f2df35d2eaf3ec8442e
apurvparekh30 <apurvparekh30@gmail.com>
2018-06-15 11:49:22 -0400

    The trip and Dragon of Loowater

---
+++ solutions/p12210.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int B,S;
+    while(scanf("%d %d",&B,&S), B||S) {
+        int bachlors[B];
+        int spinster[S];
+        for(int i=0;i<B;i++){
+            scanf("%d",&bachlors[i]);
+        }
+        for(int i=0;i<S;i++){
+            scanf("%d",&spinster[i]);
+        }
+        
+    }
+    return 0;
+}
--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,31 +1,43 @@
 #include <cstdio>
-#include <cmath>
-
+#include <cstring>
+#include <algorithm>
+#define MAX 10100
 using namespace std;
 
-int main(){
-    int T,p,q,r,s,t,u;
-    double lo,hi,mi,f;
-    
-    while(scanf("%d %d %d %d %d %d",&p,&q,&r,&s,&t,&u)==6){
-        if(p*exp(-1)+q*sin(1)+r*cos(1)+s*tan(1)+t+u>1e-9 || p+r+u<0){
-            printf("No solution\n");
-            continue;
-        }
-        
-        lo=0.0; hi=1.0;
-        
-        for(int i=0;i<30;i++){
-            mi=(lo+hi)/2;
-            
-            f=p*exp(-mi)+q*sin(mi)+r*cos(mi)+s*tan(mi)+t*mi*mi+u;
-            
-            if(f>0) lo=mi;
-            else hi=mi;
-        }
-        
-        printf("%.4f\n",lo);
-    }
-    
-    return 0;
+int store[MAX];
+
+int main() {
+	int n;
+	while ((scanf("%d",&n)==1) && (n>0)) {
+		memset(store,0,sizeof(store));
+		for(int i=0;i<n;i++){
+			scanf("%d",&store[i]);
+		}
+		sort(store,store+n);
+
+		// Find the maximum number of same sized bags
+		int maxLug = -1;
+		int curr = 1;
+		for(int i=0;i<n;i++){
+			if(store[i] == store[i+1]) {
+				curr++;
+				continue;
+			}
+			if (curr > maxLug) maxLug = curr;
+			curr = 1;
+		}
+
+		// Output with the max num of same sized bags interval
+		printf("%d\n",maxLug);
+		for (int i=0;i<maxLug;i++){
+			bool first = true;
+			for (int j=i;j<n;j+=maxLug) {
+				if (first) first = false;
+				else  printf(" ");
+				printf("%d",store[j]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
 }
