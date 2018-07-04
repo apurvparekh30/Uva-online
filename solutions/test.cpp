bf5815acbee1569febd9cee68f35e6eef707a790
apurvparekh30 <apurvparekh30@gmail.com>
2018-07-04 12:07:19 -0400

    10496 Collecting Beepers

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,30 +1,42 @@
-// UVa 11517 - Exact Change
 
-#include <stdio.h>
-#include <string.h>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int MAX = 11;
+const int INF = 1000000;
+
+int x[MAX], y[MAX];
+int n;
+int memo[MAX][1 << MAX];
+
+int distance(int i, int j) {
+    return abs(x[i] - x[j]) + abs(y[i] - y[j]);
+}
+
+int tsp(int from, int mask) {
+    if (mask == (1 << n) - 1) return distance(from, 0);
+    int &v = memo[from][mask];
+    if (v == 0) {
+        v = INF;
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) continue;
+            v = min(v, distance(from, i) + tsp(i, mask | (1 << i)));
+        }
+    }
+    return v;
+}
 
 int main() {
-	int t;
-	for (scanf("%d", &t); t; t--) {
-		int to_pay;
-		scanf("%d", &to_pay);
-		int n;
-		scanf("%d", &n);
-		int T[10001];
-		memset(T, 127, sizeof(T));
-		int oo = T[0];
-		T[0] = 0;
-		for (int i = 0; i < n; i++) {
-			int coin;
-			scanf("%d", &coin);
-			for (int j = 10000 - coin; j >= 0; j--)
-				if (T[j] != oo && T[j] + 1 < T[j + coin])
-					T[j + coin] = T[j] + 1;
-		}
-		while (T[to_pay] == oo)
-			to_pay++;
-		printf("%d %d\n", to_pay, T[to_pay]);
-	}
-
-	return 0;
+    int tcc;
+    cin >> tcc;
+    while (tcc--) {
+        cin >> x[0] >> x[0] >> x[0] >> y[0];
+        cin >> n;
+        n++;
+        for (int i = 1; i < n; i++) cin >> x[i] >> y[i];
+        fill(&memo[0][0], &memo[MAX][0], 0);
+        printf("The shortest path has length %d\n", tsp(0, 1));
+    }
 }
