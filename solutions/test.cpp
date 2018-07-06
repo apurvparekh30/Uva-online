70fdb37aefd8f19a41fd1b3838beaff070746f83
apurvparekh30 <apurvparekh30@gmail.com>
2018-07-06 16:06:06 -0400

    12442 - Forwarding Emails

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,67 +1,105 @@
-#include <stdio.h>
-#include <string.h>
-#define MAX 102
+#include <cstdio>
 
-int T,R,C,M,N,W;
-int g[MAX][MAX], v[MAX][MAX], c[MAX][MAX];
+const int R_max = 100, C_max = 100;
 
-int incr[2]={1,-1};
-void dfs(int x, int y){
-    int i,j,m,n;
-    if(v[x][y]) 
-        return;
-    v[x][y]=1;
-    /*printf("dfs %d %dn",x,y);*/
-    for(i=0; i<2;i++){
-        for(j=0;j<2;j++){
-            m = x+incr[i]*M; n = y+incr[j]*N;
-            if(m>=0 && m < R && n>=0 && n<C && g[m][n]!=-1){
-                /*printf("   x%d %dn",m,n);*/
-                c[m][n]++;
-                dfs(m,n);
-            }
-            m = x+incr[i]*N; n = y+incr[j]*M;
-            if(m>=0 && m < R && n>=0 && n<C && g[m][n]!=-1){
-                /*printf("   x%d %dn",m,n);*/
-                c[m][n]++;
-                dfs(m,n);
-            }
-        }
+int R, C, M, N;
+bool visited[R_max][C_max];
+int grid[R_max][C_max];
+  // grid[r][c] is the number of squares from which the knight can reach grid[r][c]
+  // -1 for the ones that the knight cannot reach, -2 for the ones containing water
+
+void dfs(int r, int c)
+{
+  visited[r][c] = true;
+  const int nr_dirs = 4;
+  const int dirs[nr_dirs][2] = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+  int ctr = 0;
+  if (!M) {
+    for (int i = 0; i < nr_dirs / 2; i++) {
+      int nr = r + N * dirs[i][0];
+      if (nr >= 0 && nr < R && grid[nr][c] != -2) {
+        ctr++;
+        if (!visited[nr][c])
+          dfs(nr, c);
+      }
     }
+    for (int i = 0; i < nr_dirs / 2; i++) {
+      int nc = c + N * dirs[i][0];
+      if (nc >= 0 && nc < C && grid[r][nc] != -2) {
+        ctr++;
+        if (!visited[r][nc])
+          dfs(r, nc);
+      }
+    }
+  }
+  else if (!N) {
+    for (int i = 0; i < nr_dirs / 2; i++) {
+      int nr = r + M * dirs[i][0];
+      if (nr >= 0 && nr < R && grid[nr][c] != -2) {
+        ctr++;
+        if (!visited[nr][c])
+          dfs(nr, c);
+      }
+    }
+    for (int i = 0; i < nr_dirs / 2; i++) {
+      int nc = c + M * dirs[i][0];
+      if (nc >= 0 && nc < C && grid[r][nc] != -2) {
+        ctr++;
+        if (!visited[r][nc])
+          dfs(r, nc);
+      }
+    }
+  }
+  else {
+    for (int i = 0; i < nr_dirs; i++) {
+      int nr = r + M * dirs[i][0], nc = c + N * dirs[i][1];
+      if (nr >= 0 && nr < R && nc >= 0 && nc < C && grid[nr][nc] != -2) {
+        ctr++;
+        if (!visited[nr][nc])
+          dfs(nr, nc);
+      }
+    }
+    if (M != N)
+      for (int i = 0; i < nr_dirs; i++) {
+        int nr = r + N * dirs[i][0], nc = c + M * dirs[i][1];
+        if (nr >= 0 && nr < R && nc >= 0 && nc < C && grid[nr][nc] != -2) {
+          ctr++;
+          if (!visited[nr][nc])
+            dfs(nr, nc);
+        }
+      }
+  }
+  grid[r][c] = ctr;
 }
 
-int main(){
-    int t,i,j,even,odd;
-    scanf("%d",&T);
-    for(t=1;t<=T;t++){
-        /* clean-up */
-        memset(v,0,sizeof v);
-        memset(g,0,sizeof g);
-        memset(c,0,sizeof c);
-        even=odd=0;
-        /* Input Begin */
-        scanf("%d %d %d %d %d",&R,&C,&M,&N,&W);
-        while(W--){
-            scanf("%d %d",&i,&j);
-            g[i][j] = -1;
-        }
-        /* Input end */
-        dfs(0,0);
-        /* Count evens and odds */
-        for(i=0;i<R;i++) {
-            for(j=0;j<C;j++){
-                /* We counted twice */
-                if(M==0||N==0||M==N)
-                    c[i][j]/=2;
-                /*printf("%d ",c[i][j]);*/
-                if(c[i][j] || (!i && !j)) { 
-                    if(c[i][j] % 2 == 0) even++; 
-                    else odd++;
-                }
-            }
-            /*printf("n");*/
-        }
-        printf("Case %d: %d %d\n",t,even,odd);
+int main()
+{
+  int T;
+  scanf("%d", &T);
+  for (int t = 1; t <= T; t++) {
+    int W;
+    scanf("%d %d %d %d %d", &R, &C, &M, &N, &W);
+    for (int r = 0; r < R; r++)
+      for (int c = 0; c < C; c++) {
+        visited[r][c] = false;
+        grid[r][c] = -1;
+      }
+    while (W--) {
+      int r, c;
+      scanf("%d %d", &r, &c);
+      grid[r][c] = -2;
     }
-    return 0;
+    dfs(0, 0);
+    int even = 0, odd = 0;
+    for (int r = 0; r < R; r++)
+      for (int c = 0; c < C; c++)
+        if (grid[r][c] >= 0) {
+          if (grid[r][c] & 1)
+            odd++;
+          else
+            even++;
+        }
+    printf("Case %d: %d %d\n", t, even, odd);
+  }
+  return 0;
 }
