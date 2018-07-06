15d7aff65eadaf5ce742ff48ea70cb8e8bb4cb0a
apurvparekh30 <apurvparekh30@gmail.com>
2018-07-06 14:00:52 -0400

    11906 - Knight in a War Grid

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,91 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 102
 
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <deque>
-#include <stack>
-#include <bitset>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
+int T,R,C,M,N,W;
+int g[MAX][MAX], v[MAX][MAX], c[MAX][MAX];
 
-using namespace std;
-
-const int MAX_N = 51;
-const int MAX_P = 13;
-const int INF = 10000000;
-
-int D[MAX_N][MAX_N];
-int memo[MAX_P][1 << MAX_P];
-int index[MAX_P], value[MAX_P];
-int n, p;
-
-void fw() {
-    for (int i = 0; i < n; i++) D[i][i] = 0;
-    for (int k = 0; k < n; k++)
-    for (int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++)
-        D[i][j] = min(D[i][j], D[i][k] + D[k][j]);
-}
-
-int tsp(int idx, int mask) {
-    if (mask == (1 << p) - 1) return -D[0][index[idx]];
-    int &v = memo[idx][mask];
-    if (v == -1) {
-        v = -D[0][index[idx]];
-        for (int i = 0; i < p; i++) {
-            if (mask & (1 << i)) continue;
-            int t = value[i]
-                    - D[index[i]][index[idx]]
-                    + tsp(i, mask | (1 << i));
-            v = max(v, t);
+int incr[2]={1,-1};
+void dfs(int x, int y){
+    int i,j,m,n;
+    if(v[x][y]) 
+        return;
+    v[x][y]=1;
+    /*printf("dfs %d %dn",x,y);*/
+    for(i=0; i<2;i++){
+        for(j=0;j<2;j++){
+            m = x+incr[i]*M; n = y+incr[j]*N;
+            if(m>=0 && m < R && n>=0 && n<C && g[m][n]!=-1){
+                /*printf("   x%d %dn",m,n);*/
+                c[m][n]++;
+                dfs(m,n);
+            }
+            m = x+incr[i]*N; n = y+incr[j]*M;
+            if(m>=0 && m < R && n>=0 && n<C && g[m][n]!=-1){
+                /*printf("   x%d %dn",m,n);*/
+                c[m][n]++;
+                dfs(m,n);
+            }
         }
     }
-    return v;
 }
 
-int main() {
-    int tcc;
-    cin >> tcc;
-    while (tcc--) {
-        int m;
-        cin >> n >> m;
-        n++;
-        // cerr << "n " << n << " m " << m << endl;
-        fill(&D[0][0], &D[MAX_N][0], INF);
-        while (m--) {
-            int a, b;
-            double c;
-            cin >> a >> b >> c;
-            int t = round(c * 100);
-            t = min(t, D[a][b]);
-            D[a][b] = D[b][a] = t;
+int main(){
+    int t,i,j,even,odd;
+    scanf("%d",&T);
+    for(t=1;t<=T;t++){
+        /* clean-up */
+        memset(v,0,sizeof v);
+        memset(g,0,sizeof g);
+        memset(c,0,sizeof c);
+        even=odd=0;
+        /* Input Begin */
+        scanf("%d %d %d %d %d",&R,&C,&M,&N,&W);
+        while(W--){
+            scanf("%d %d",&i,&j);
+            g[i][j] = -1;
         }
-        fw();
-        cin >> p;
-        p++;
-        value[0] = 0;
-        index[0] = 0;
-        for (int i = 1; i < p; i++) {
-            double c;
-            cin >> index[i] >> c;
-            value[i] = round(c * 100);
+        /* Input end */
+        dfs(0,0);
+        /* Count evens and odds */
+        for(i=0;i<R;i++) {
+            for(j=0;j<C;j++){
+                /* We counted twice */
+                if(M==0||N==0||M==N)
+                    c[i][j]/=2;
+                /*printf("%d ",c[i][j]);*/
+                if(c[i][j] || (!i && !j)) { 
+                    if(c[i][j] % 2 == 0) even++; 
+                    else odd++;
+                }
+            }
+            /*printf("n");*/
         }
-        fill(&memo[0][0], &memo[MAX_P][0], -1);
-        int best = tsp(0, 1);
-        if (best > 0) {
-            printf("Daniel can save $%.2f\n", double(best) / 100);
-        } else {
-            printf("Don't leave the house\n");
-        }
+        printf("Case %d: %d %d\n",t,even,odd);
     }
+    return 0;
 }
