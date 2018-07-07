d5ccd432dabe13264d97d5aafe60ba6b8ccd3a6c
apurvparekh30 <apurvparekh30@gmail.com>
2018-07-07 12:35:58 -0400

    1103 Ancient Messages

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,105 +1,92 @@
-#include <cstdio>
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<queue>
+#include<map>
+#include<cstring>
 
-const int R_max = 100, C_max = 100;
+#define fr(i,j,k) for(int (i)=(j);(i)<(k);++(i))
+#define cl(i) memset(i,0,sizeof(i))
+#define F first
+#define S second
+#define pii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define PI acos(-1)
+#define db(x) if(1) cerr << #x << " = " << x << endl;
+#define _ << ", " << 
+#define ll long long
 
-int R, C, M, N;
-bool visited[R_max][C_max];
-int grid[R_max][C_max];
-  // grid[r][c] is the number of squares from which the knight can reach grid[r][c]
-  // -1 for the ones that the knight cannot reach, -2 for the ones containing water
+using namespace std;
 
-void dfs(int r, int c)
-{
-  visited[r][c] = true;
-  const int nr_dirs = 4;
-  const int dirs[nr_dirs][2] = {{1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
-  int ctr = 0;
-  if (!M) {
-    for (int i = 0; i < nr_dirs / 2; i++) {
-      int nr = r + N * dirs[i][0];
-      if (nr >= 0 && nr < R && grid[nr][c] != -2) {
-        ctr++;
-        if (!visited[nr][c])
-          dfs(nr, c);
-      }
-    }
-    for (int i = 0; i < nr_dirs / 2; i++) {
-      int nc = c + N * dirs[i][0];
-      if (nc >= 0 && nc < C && grid[r][nc] != -2) {
-        ctr++;
-        if (!visited[r][nc])
-          dfs(r, nc);
-      }
-    }
-  }
-  else if (!N) {
-    for (int i = 0; i < nr_dirs / 2; i++) {
-      int nr = r + M * dirs[i][0];
-      if (nr >= 0 && nr < R && grid[nr][c] != -2) {
-        ctr++;
-        if (!visited[nr][c])
-          dfs(nr, c);
-      }
-    }
-    for (int i = 0; i < nr_dirs / 2; i++) {
-      int nc = c + M * dirs[i][0];
-      if (nc >= 0 && nc < C && grid[r][nc] != -2) {
-        ctr++;
-        if (!visited[r][nc])
-          dfs(r, nc);
-      }
-    }
-  }
-  else {
-    for (int i = 0; i < nr_dirs; i++) {
-      int nr = r + M * dirs[i][0], nc = c + N * dirs[i][1];
-      if (nr >= 0 && nr < R && nc >= 0 && nc < C && grid[nr][nc] != -2) {
-        ctr++;
-        if (!visited[nr][nc])
-          dfs(nr, nc);
-      }
-    }
-    if (M != N)
-      for (int i = 0; i < nr_dirs; i++) {
-        int nr = r + N * dirs[i][0], nc = c + M * dirs[i][1];
-        if (nr >= 0 && nr < R && nc >= 0 && nc < C && grid[nr][nc] != -2) {
-          ctr++;
-          if (!visited[nr][nc])
-            dfs(nr, nc);
-        }
-      }
-  }
-  grid[r][c] = ctr;
+
+
+char in[401][51];
+int grid[410][410];
+int val[210];
+char resp[2000];
+
+int n ,m ;
+
+void paint(int y, int x, int c, int d ){
+	if( grid[y][x] != d || y < 0 || y > n || x < 0 || x > m ) return;
+
+	grid[y][x] = c;
+	paint( y -1, x , c, d);
+	paint( y, x-1 , c, d);
+	paint( y +1, x , c, d);
+	paint( y , x + 1 , c, d);
 }
 
-int main()
-{
-  int T;
-  scanf("%d", &T);
-  for (int t = 1; t <= T; t++) {
-    int W;
-    scanf("%d %d %d %d %d", &R, &C, &M, &N, &W);
-    for (int r = 0; r < R; r++)
-      for (int c = 0; c < C; c++) {
-        visited[r][c] = false;
-        grid[r][c] = -1;
-      }
-    while (W--) {
-      int r, c;
-      scanf("%d %d", &r, &c);
-      grid[r][c] = -2;
-    }
-    dfs(0, 0);
-    int even = 0, odd = 0;
-    for (int r = 0; r < R; r++)
-      for (int c = 0; c < C; c++)
-        if (grid[r][c] >= 0) {
-          if (grid[r][c] & 1)
-            odd++;
-          else
-            even++;
-        }
-    printf("Case %d: %d %d\n", t, even, odd);
-  }
-  return 0;
+int main(){
+	
+	int caso = 1;
+	while(scanf("%d%d", &n ,&m ) && (n||m)){
+		cl( grid); cl( val); cl( resp);
+		fr(i,0,n){
+			scanf("%s", in[i]);
+		}
+		fr(i,0,n){
+			fr(j,0,m){
+				int aux = ( in[i][j]  >= '0' && in[i][j] <= '9')? (in[i][j] - '0') : (in[i][j] - 'a' + 10);
+				int pos = 3;
+				// cout << aux << endl;
+				while(aux > 0) {
+					grid[i][j*4+pos] = (aux & 1);
+					aux >>= 1; pos--;
+				}
+			}
+		}
+		m *= 4 ;  int ini = 2; bool out = true;
+		// fr(i,0,n) { fr(j,0,m) { cout << grid[i][j]; } cout << endl; } cout << endl;
+		fr(i,0,n) { fr(j,0,m) { if( i == 0 || i == n-1 ||  j == 0 || j == m-1 ) paint( i, j, 10000, 0); } }
+		// fr(i,0,n) { fr(j,0,m) { cout << grid[i][j]; } cout << endl; } cout << endl;
+		int base = ini;
+		fr(i,0,n){
+			fr(j,0,m){
+				if( grid[i][j] == 0){ 
+					val[ grid[i][j-1] ]++;
+					paint( i, j , grid[i][j-1], 0);
+				} else if( grid[i][j]  == 1){
+					paint( i, j , base++, 1);
+				}				
+			}
+		}
+		int num = 0;
+		fr(i,ini,base){
+			if( val[i] == 0) resp[num++] = 'W';
+			if( val[i] == 1) resp[num++] = 'A';
+			if( val[i] == 2) resp[num++] = 'K';
+			if( val[i] == 3) resp[num++] = 'J';
+			if( val[i] == 4) resp[num++] = 'S';
+			if( val[i] == 5) resp[num++] = 'D';
+		}
+		sort( resp, resp + num);
+		printf("Case %d: %s\n", caso++, resp);
+		
+	}
+	return 0;
 }
