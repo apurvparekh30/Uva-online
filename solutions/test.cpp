9eb866af326d225c146ffe4d9e7753581d3f1dec
apurvparekh30 <apurvparekh30@gmail.com>
2018-08-11 12:28:12 -0400

    10646 What is the Card?

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,48 +1,26 @@
-// Apurv Parekh
-// 8th Aug 2018
-
-
-#include <algorithm>
-#include <cstdio>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-const int maxx = 200;
-
-int inDec[maxx];
-int outDec[maxx];
-
-int n,r;
-
 int main(){
-    while(scanf("%d%d",&n,&r)==2){
-        fill(inDec,inDec+maxx,0);
-        fill(outDec,outDec+maxx,0);
-        while(r--){
-            int u,v;
-            scanf("%d%d",&u,&v);
-            outDec[u]++;
-            inDec[v]++;
-        }
-        int bad=false;
-        int inDiff=0;
-        int outDiff=0;
-        for(int i=0;i<n;i++){
-            int res = inDec[i]-outDec[i];
-            if(res==0) continue;
-            else if(res>1 || res<-1){
-                bad=true;
-                break;
-            }
-            else if(res==1) inDiff++;
-            else if(res==-1) outDiff++;
-        }
-        if(bad || inDiff>1 || outDiff>0){
-            printf("Not Possible\n");
-        }
-        else{
-             printf("Possible\n");
-        }
-    }
+	int n;
+	while (cin >> n && n){
+		string s;
+		int x=1,y=0,z=0;
+		for (int i=0;i<n-1;i++){
+			cin >> s;
+			if (s[0]=='N') continue;
+			else if (s=="+y") swap(x,y), x=-x; // x=-y, y=x;
+			else if (s=="-y") swap(x,y), y=-y; // x=y, y=-x;
+			else if (s=="+z") swap(x,z), x=-x; // x=-z, z=x;
+			else swap(x,z), z=-z; // x=z, z=-x; 
+		}
+		// cout << x << " " << y << " " << z << endl;
+		if (x+y+z<0) cout << "-";
+		else cout << "+";
+		if (x) cout << "x";
+		else if (y) cout << "y";
+		else cout << "z";
+		cout << endl;
+	}
     return 0;
 }
