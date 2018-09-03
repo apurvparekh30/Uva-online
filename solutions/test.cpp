270316d10f5f1704de2feee5b46f715c035dec6f
apurvparekh30 <apurvparekh30@gmail.com>
2018-09-03 00:34:37 -0400

    893Y3K Problem

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,22 +1,70 @@
-// UVa 10812 - Beat the Spread!
-
 #include <iostream>
+
 using namespace std;
 
-#define type signed long long
+// Months are 0 indexed
 
-int main() {
-	int cases;
-	for (cin >> cases; cases; cases--) {
-		type s, d;
-		cin >> s >> d;
-		type x, y;
-		x = (s + d) / 2;
-		y = x - d;
-		if ((s + d) % 2 == 1 || x < 0 || y < 0)
-			cout << "impossible" << endl;
-		else
-			cout << x << " " << y << endl;
-	}
-	return 0;
+int numberDaysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int numberDaysInMonthLeap[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+bool IsLeapYear(int year)
+{
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
+int MonthToDay(int month, int year)
+{
+    int daysBefore = 0;
+    for (int i = 0; i < month; ++i)
+        daysBefore += numberDaysInMonth[i];
+    if (month > 1 && IsLeapYear(year))
+        ++daysBefore;
+    return daysBefore;
+}
+
+int YearToDay(int year)
+{
+    int base = year * 365;
+    int numLeapYears = year / 4 - year / 100 + year / 400;
+    return base + numLeapYears;
+}
+
+int GetYearFromNumDays(int& numDays)
+{
+    int year = 1;
+    int sizeOfYear = 365;
+    
+    while (numDays > sizeOfYear)
+    {
+        numDays -= sizeOfYear;
+        ++year;
+        sizeOfYear = (IsLeapYear(year)) ? 366 : 365;
+    }
+    
+    return year;
+}
+
+int GetMonthFromNumDays(int& numDays, int year)
+{
+    int month = 0;
+    int * numDayUsed = (IsLeapYear(year)) ? numberDaysInMonthLeap : numberDaysInMonth;
+    for (;numDays > numDayUsed[month]; ++month)
+        numDays -= numDayUsed[month];
+    return month + 1;
+}
+
+int main()
+{
+    int dayForward, day, month, year;
+    while (cin >> dayForward >> day >> month >> year, year)
+    {
+        --month; --year;
+        day += MonthToDay(month, year);
+        day += YearToDay(year);
+        day += dayForward;
+		cout<<day<<endl;
+        year = GetYearFromNumDays(day);
+        month = GetMonthFromNumDays(day, year);
+        //cout << day << ' ' << month << ' ' << year << '\n';
+    }
 }
