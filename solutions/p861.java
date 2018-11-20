4a61bfe1847a667312116d92e15e26db75662148
apurvparekh30 <apurvparekh30@gmail.com>
2018-11-20 13:19:29 -0500

    1193 Radar Installation

--- solutions/p861.java
+++ solutions/p861.java
@@ -6,6 +6,7 @@ class Main {
     static FastReader fs = new FastReader();
     static int n,k;
     static int cnt = 0;
+    static int res[][] = new int[9][65];
 
 
     static void rec(int c,int r,int fd,int bd,int k){
@@ -13,25 +14,38 @@ class Main {
             cnt++;
             return;
         }
+        int used = Math.max(c-1*n,0) + r;
+        //System.out.println();
+        if((n*n-used) < k)
+            return;
         if(c==n)
             return;
         for(int i=r;i<n;i++){
-            if(((fd & (1<<(r+c)))==0) && ((bd & (1<<(r-c+n)))==0)){
-                rec(c,r+1,fd|(1<<(r+c)),bd|(1<<(r-c+n)),k-1);
+            if(((fd & (1<<(i+c)))==0) && ((bd & (1<<(i-c+n)))==0)){
+                rec(c,i+1,fd|(1<<(i+c)),bd|(1<<(i-c+n)),k-1);
             }
         }
         rec(c+1,0,fd,bd,k);
     }
 
     public static void main(String[] args) {
+
+        for(int i=1;i<=8;i++){
+            n = i;
+            for(int j=1;j<=(i*i);j++){
+                cnt = 0;
+                rec(0,0,0,0,j);
+                res[i][j] = cnt;
+            }
+        }
         while(true){
             n = fs.nextInt();
             k = fs.nextInt();
             if(n==0)
                 break;
-            cnt=0;
+            cnt = 0;
             rec(0,0,0,0,k);
-            System.out.println(cnt);
+            System.out.println(res[n][k]);
         }
     }
 
