2d9c0dcfed0ebdb9c60874a2bdff0a56f94e9d92
apurvparekh30 <apurvparekh30@gmail.com>
2018-11-21 19:36:28 -0500

    10382 Watering Grass

--- solutions/test.cpp
+++ solutions/test.cpp
@@ -1,66 +1,67 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
 #include <vector>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
-#include <cstdlib>
+#include <algorithm>
 #include <cmath>
-#include <bitset>
-
 using namespace std;
 
-#define Max 10000
-const double eps = 1e-10;
-
-struct Interval {
-    double st, en;
-    Interval() {}
-    Interval(double s, double e) {
-        st = s, en = e;
-    }
-    bool operator < (const Interval &i) const {
-        return (i.en == en) ? (st < i.st) : (en < i.en);
-    }
+struct Interval
+{
+    double L, R;
 };
-long double x[Max], y[Max];
-Interval inter[Max];
 
-//bujhlam na baal
-int main(void) {
-    int n, d, testcase = 0;
-    while(scanf("%d %d", &n, &d) == 2 && !(n == 0 && d == 0)) {
-        for(int i = 0; i < n; i++)
-            scanf("%Lf %Lf", &x[i], &y[i]);
-        int count = 0, ok = true;
-        for(int i = 0; i < n; i++) {
-            if(d < y[i]) { // if island is out of radar radious
-                ok = false; // that means at least one of the islands is not reachable results in -1
-                break;
-            } else {
-                long double sqrtd = sqrt( d * d - y[i] * y[i] );
-                inter[i] = Interval(x[i] - sqrtd, x[i] + sqrtd);
-            }
-        }
-        if(!ok) {
-            printf("Case %d: %d\n", ++testcase, -1);
-            continue;
-        }
-        sort(inter, inter + n);
+bool comp(const Interval &i1, const Interval &i2)
+{
+    return i1.L < i2.L;
+}
 
-        for(int i = 0; i < n;) {
-            int j;
-            for(j = 0; j < n; j++) {
-                if(inter[j].st > inter[i].en)
+int main()
+{  
+    size_t n, l, w;
+    while (cin >> n >> l >> w)
+    {
+        vector<Interval> circles(1);
+        for (size_t i = 0; i < n; ++i)
+        {
+            double pos, radius;
+            cin >> pos >> radius;
+            // Calculate this circle's effective interval [L, R].
+            // Then this problem is identical to 10020 - Minimal coverage.
+            double range = sqrt(radius * radius - (w / 2.0) * (w / 2.0));
+            circles[0].L = pos - range;
+            circles[0].R = pos + range;
+            circles.push_back(circles[0]);
+        }
+        // Sort the circles by increasing left endpoint. 
+        sort(circles.begin() + 1, circles.end(), comp);
+        for(int i=0;i<n;i++)
+            cout << circles[i].L << " " << circles[i].R << "\n";
+        double curL = 0, rReach = 0;
+        size_t i = 1;
+        size_t nSprinklers = 0; 
+        while (rReach < l)
+        {
+            double newCurL = curL;
+            size_t farthest = 0;
+            // Take the interval that covers as far right as possible.
+            for (; i < circles.size(); ++i)
+            {
+                if (circles[i].L > curL)
                     break;
+                if (circles[i].R >= newCurL)
+                {
+                    newCurL = circles[i].R;
+                    farthest = i;
+                }
             }
-            i = j;
-            count++;
+            if (farthest == 0)
+                break;
+            ++nSprinklers;
+            rReach = curL = newCurL;
         }
-        printf("Case %d: %d\n", ++testcase, count);
+        if (rReach < l)
+            cout << "-1" << endl;
+        else
+            cout << nSprinklers << endl;
     }
     return 0;
 }
